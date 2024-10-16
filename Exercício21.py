ea7c78383b7427457b2e7d3887e3e31e3d464e58
BRCMOURA <brcmoura@gmail.com>
2024-10-16 18:54:48 -0300

    Add files via upload

---
+++ Exercício21.py
@@ -0,0 +1,13 @@
+#Diferença entre dois números
+
+num1 = int(input("Digite um número:"))
+
+num2 = int(input("Digite outro número:"))
+
+if num1 > num2:
+    diferenca = num1 - num2
+
+else:
+    diferenca = num2 - num1
+
+print(f'A diferença entre os dois números é:', diferenca)
